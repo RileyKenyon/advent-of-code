703f6252ddb162fc5d20d43a9135baee129ae93a
RileyKenyon <rike2277@colorado.edu>
2021-12-04 22:47:34 -0700

    AOC: 01 Part 2

--- 01/main.py
+++ 01/main.py
@@ -2,6 +2,7 @@ import math
 import sys
 import os
 
+sliding_window = 3
 
 def main():
     fname = sys.argv[1]
@@ -9,8 +10,10 @@ def main():
         lines = f.read().splitlines()
         depth = [int(line) for line in lines]
         count = 0
-        for i in range(0, len(depth) - 1):
-            count = count + 1 if depth[i+1] > depth[i] else count
+        for i in range(0, len(depth) - sliding_window):
+            sum1 = sum(depth[i + 1 : i + 1 + sliding_window])
+            sum2 = sum(depth[i : i+sliding_window])
+            count = count + 1 if sum1 > sum2 else count
 
     print(count)
 
