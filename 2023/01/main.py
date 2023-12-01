9781d0a49f56a0066070794f68ac80c8acd517b8
Riley Kenyon <rike2277@colorado.edu>
2023-11-30 23:52:36 -0700

    02

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,20 +1,64 @@
 import math
 import sys
 import os
-import re
+import regex as re
+
+lut = {
+  'one' : '1',
+  'two' : '2',
+  'three' : '3',
+  'four' : '4',
+  'five' : '5',
+  'six' : '6',
+  'seven' : '7',
+  'eight' : '8',
+  'nine' : '9',
+  '1' : '1',
+  '2' : '2',
+  '3' : '3',
+  '4' : '4',
+  '5' : '5',
+  '6' : '6',
+  '7' : '7',
+  '8' : '8',
+  '9' : '9',  
+} 
+
 def main():
     fname = sys.argv[1]
     with open(fname,'r') as f:
         lines = f.read().splitlines()
         sum = 0
+        it = 1
         for line in lines:
-            value = re.sub(r'[a-z]|[A-Z]','',line)
-            if (len(line) > 1):
-                value = int(value[0] + value[-1])
+            matches = re.findall(r'(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\d)',line, overlapped=True)
+            # matches = re.findall(r'(\d)',line)
+            print(matches)
+            tmp = ''
+            if (len(matches) > 1):
+                for m in matches[0]:
+                    if m:
+                        tmp+=lut[m]
+                        print(lut[m])
+                for m in matches[-1]:
+                    if m:
+                        tmp+=lut[m]
+                        print(lut[m])
             else:
-                value = int(value)
+                for m in matches[0]:
+                  if m:
+                      print(lut[m])
+                      if (len(lut[m]) == 1):
+                          tmp+=lut[m]
+                          tmp+=lut[m]
+                      else:
+                          tmp+=lut[m]
+                      # print(lut[m])
+            value = int(tmp)
+            print(it, value)
+            it+=1
             sum+=value
-        print(sum)
+        print(sum) # too low: 51890, not correct: 54780
 
 if __name__ == "__main__":
     main()
