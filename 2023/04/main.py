249697bf1c1e3c0b6a6832b0ce52197291dee7c9
Riley Kenyon <rike2277@colorado.edu>
2023-12-03 22:43:28 -0700

    04: Part 2

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -10,26 +10,19 @@ def main():
         lines = f.read().splitlines()
         lines = [line.split(':')[1].split('|') for line in lines]
         both = [[line[0].strip().split(' '), line[1].strip().split(' ')] for line in lines]
-        total_score = 0
-        for pair in lines:
-            winning = pair[0].strip().split(' ')
-            winning = [value if value != '' else [] for value in winning]
-            my_numbers = pair[1].strip().split(' ')
-            score = 0
-            matches = []
-            for num in my_numbers: 
-                if num in winning:
-                    matches.append(num)
-                    score = 1 if score == 0 else score*2
-            total_score+=score
-            print("Score: ", score, matches)
-        print("total", total_score)
-
+        number_of_cards = [1]* len(lines)
+        for id, pair in enumerate(lines):
+            for _ in range(0, number_of_cards[id]):
+              winning = pair[0].strip().split(' ')
+              winning = [value if value != '' else [] for value in winning]
+              my_numbers = pair[1].strip().split(' ')
+              count = 0
+              for num in my_numbers: 
+                  if num in winning:
+                      count+=1
+                      if (count + id < len(lines)):
+                        number_of_cards[count + id]+=1
+        print("total", sum(number_of_cards))
         
-        # first match is one point
-        # Subsequent matches doubles the point value of the card
-        # 159074 - not right
-
-
 if __name__ == "__main__":
     main()
