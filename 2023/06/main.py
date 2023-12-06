5ae6509eab11e665eb8fdd2c45de98a8ae8e53d6
Riley Kenyon <rike2277@colorado.edu>
2023-12-05 22:29:20 -0700

    Day 06: Part 2

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -7,17 +7,17 @@ def main():
     fname = sys.argv[1]
     with open(fname,'r') as f:
         lines = f.read().splitlines()
-        lines = [list(filter(lambda x: (x if x != '' else []), line.split(':')[1].strip().split(' '))) for line in lines]
+        time, distance = [line.split(':')[1].replace(' ', '') for line in lines]
+        print(time, distance)
         total = 1
-        for time, distance in zip(lines[0], lines[1]):
-            r1 = range(0, int(time) + 1) 
-            r2 = reversed(range(0, int(time) + 1))
-            number_of_ways = 0
-            for [speed, travel] in zip(r1, r2):
-                if speed * travel > int(distance):
-                    number_of_ways+=1
-            print(number_of_ways)
-            total*=number_of_ways 
+        r1 = range(0, int(time) + 1) 
+        r2 = reversed(range(0, int(time) + 1))
+        number_of_ways = 0
+        for [speed, travel] in zip(r1, r2):
+            if speed * travel > int(distance):
+                number_of_ways+=1
+        print(number_of_ways)
+        total*=number_of_ways 
         print("total: ", total)
         
 if __name__ == "__main__":
