481f6affd5af29e8a42249ad2a73e5723c3a3a08
Riley Kenyon <rike2277@colorado.edu>
2023-12-07 22:50:21 -0700

    Day 08: Part 2 LCM

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -3,7 +3,11 @@ import sys
 import os
 import re
 from collections import namedtuple
+from functools import reduce
+from math import gcd
 
+def lcm(nums):
+    return reduce(lambda x,y: x*y // gcd(x,y), nums)
 
 def main():
     fname = sys.argv[1]
@@ -26,13 +30,20 @@ def main():
         search_keys = list(filter(lambda x: x.endswith('A'), network.keys()))
 
         # do sequence until reaching 'ZZZ'
-        count = 0
-        while not all([search.endswith('Z') for search in search_keys]):
-            for lookup in sequence: 
-                count+=1
-                for id, search in enumerate(search_keys):
-                    search_keys[id] = network[search][0 if lookup == 'L' else 1]
-        print(count)
+        counters = [0] * len(search_keys)
+        for id, search in enumerate(search_keys):
+            count = 0
+            while not search.endswith('Z'):
+                for lookup in sequence: 
+                    count+=1
+                    search = network[search][0 if lookup == 'L' else 1]
+                    if search.endswith('Z'):
+                        break
+                print(search)
+            counters[id] = count
+
+        # Find lcm
+        print(lcm(counters))
 
 
         
