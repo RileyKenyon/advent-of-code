9512fef8c12cbb71a6f981c95116bb3c41082417
Riley Kenyon <rike2277@colorado.edu>
2023-12-10 13:47:17 -0700

    Day 10: Part 02

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -6,21 +6,21 @@ from collections import namedtuple
 from functools import reduce
 import copy
 
-# Left, Right, Up, Down ( X are 7)
+# Left, Right, Up, Down ( Z are 7)
 compatible = {
-    'F' : [[], ['J', 'X', '-'], [], ['J', 'L','|']],
-    'J' : [['F', 'L', '-'], [], ['|', 'F', 'X'], []],
-    'X' : [['F', 'L', '-'], [], [], ['|', 'J', 'L']],
-    'L' : [[], ['J', 'X', '-'], ['|', 'F', 'X'], []],
-    '|' : [[], [], ['F', 'X','|'], ['J', 'L', '|']],
-    '-' : [['F', 'L','-'], ['J', 'X', '-'], [], []],
-    'S' : [['F', 'L', '-'],['J', 'X', '-'], ['F','X','|'],['J','L','|']],
+    'F' : [[], ['J', 'Z', '-'], [], ['J', 'L','|']],
+    'J' : [['F', 'L', '-'], [], ['|', 'F', 'Z'], []],
+    'Z' : [['F', 'L', '-'], [], [], ['|', 'J', 'L']],
+    'L' : [[], ['J', 'Z', '-'], ['|', 'F', 'Z'], []],
+    '|' : [[], [], ['F', 'Z','|'], ['J', 'L', '|']],
+    '-' : [['F', 'L','-'], ['J', 'Z', '-'], [], []],
+    'S' : [['F', 'L', '-'],['J', 'Z', '-'], ['F','Z','|'],['J','L','|']],
     '.' : [[],[],[],[]]
 }
 
 def get_neighbors(map, start, width):
     n = []
-    value = compatible[map[start] if not map[start].isdigit() else '.']
+    value = compatible[map[start] if not map[start].isdigit() and map[start] != 'X' else '.']
     checks = [start - 1, start + 1, start - width, start + width]
     for id, idx in enumerate(checks):
         if idx >= 0 and idx < len(map):
@@ -30,23 +30,11 @@ def get_neighbors(map, start, width):
             n.append(None)
     return n
 
-def update_map(map, start, width, value):
-    neighbors = get_neighbors(map,start, width)
-    c = copy.copy(map)
-    # print(neighbors, compatible[map[start]])
-    for idx, [p, comp] in enumerate(zip(neighbors, compatible[c[start]])):
-        if p != [] and p in comp:
-            # print(p)
-            c[start] = value
-            checks = [start - 1, start + 1, start - width, start + width]
-            return update_map(c, checks[idx], width, str(int(value) + 1))
-    return c
-
 def main():
     fname = sys.argv[1]
     with open(fname,'r') as f:
         pipe = f.read()
-        pipe = re.sub('7', 'X', pipe)
+        pipe = re.sub('7', 'Z', pipe)
         # print(pipe)
         m = [0] * len(pipe)
         width = len(pipe.split('\n')[0])
@@ -54,29 +42,34 @@ def main():
         start = re.search('S', pipe).start()
         # determine starting pipe type
         pipe = list(pipe)
+        original = copy.copy(pipe)
         next_match = get_neighbors(pipe, start, width)
         print(next_match)
-        pipe[start] = '0'
-        total =  1
+        delimeter = 'X'
+        pipe[start] = delimeter
         while not all([match is None for match in next_match]) :
             tmp = []
-            print(next_match)
             for match in next_match:
                 if match is not None:
-                    print(match)
                     neighbors = get_neighbors(pipe, match, width)
-                    pipe[match] = str(total)
+                    pipe[match] = delimeter
                     for n in neighbors: 
                         tmp.append(n)
             next_match = tmp
-            print(next_match)
-            total+=1
-        print(pipe)
-        out = 0
-        for i in range(0, len(pipe)):
-            if pipe[i].isdigit() and int(pipe[i]) > out:
-                out = int(pipe[i]) 
-        print(out)
+        # scan each line
+        pipe = list(re.sub('[^X]', '.', ''.join(pipe)))
+
+        # need to region grow
+        for i in range(0, len(pipe) // width):
+            value = '0'
+            for j in range(0, width):
+                idx = i * width + j
+                if pipe[idx] == 'X':
+                    if original[idx] in "|JL" or original[idx] == 'S':
+                        value = '0' if value == '1' else '1'
+                else:
+                    pipe[idx] = value
+        print(pipe.count('1'))
 
 
 
